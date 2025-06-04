faef9ebee0571a6fcfd281893c6d4b86c97f0e6b
MaxProger338 <maxproger338@proton.me>
2025-06-04 23:00:55 +0700

    chore: Now, logos will be download to ~/.local/tmp/spotify-logos instead /tmp/spotify-logos

--- src/notify/download-track-logo.sh
+++ src/notify/download-track-logo.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
+download_dir=~/.local/tmp/spotify-music-logos
+
+# Does the spotify work?
 ~/.local/bin/spotifyctl.sh 2>&1 > /dev/null 
 [[ $? -ne 0 ]] && exit 1;
 
-url=$(~/.local/bin/spotifyctl.sh metadata | grep 'artUrl' | cut -d '|' -f 2)
-name=$(echo "$url" | cut -d '/' -f 5)
-path=/tmp/spotify-music-logos
+url="$(~/.local/bin/spotifyctl.sh metadata | grep 'artUrl' | cut -d '|' -f 2)"
+name="$(echo "$url" | cut -d '/' -f 5)"
 
-[[ ! -d "$path" ]] && mkdir "$path"
-if [[ ! -f "${path}/${name}.jpg" ]]; then
-	wget -O "${path}/${name}.jpg" "$url" > /dev/null 2>&1
+# Creating a download dir if doesn't exist
+[[ ! -d "$download_dir" ]] && mkdir -p "$download_dir"
+
+# If the logo doesn't exist, than donload him
+if [[ ! -f "${download_dir}/${name}.jpg" ]]; then
+	wget -O "${download_dir}/${name}.jpg" "$url" > /dev/null 2>&1
 fi
 
-echo "${path}/${name}.jpg"
+echo "${download_dir}/${name}.jpg"
