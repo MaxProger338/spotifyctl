a36ed25366db633042096e4eac7b0bee405e5ae1
MaxProger338 <maxproger338@proton.me>
2025-06-05 10:10:04 +0700

    chore: Added print error messages to notify/to-svg.sh and notify/notify-spotify-music.sh; Scripts's paths were changed

--- src/notify/to-svg.sh
+++ src/notify/to-svg.sh
@@ -5,6 +5,10 @@
 input="$1"
 output="${input%.*}.svg"
 
+# If inkscape not found - exit
+[[ $(command -v inkscape) = "" ]] && echo "Inkscape not found" 1>&2 && exit 1
+
+# If file has been already converted, than doesn't convert again
 if [[ ! -f "$output" ]]; then
 	inkscape "$input" --export-plain-svg="$output" > /dev/null 2>&1
 fi
