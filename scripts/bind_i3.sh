f0f2759d297c512d4857396e43a20c3e2f0adac4
MaxProger338 <maxproger338@proton.me>
2025-06-22 17:14:12 +0700

    project: Updated scripts/bind_i3.sh

--- scripts/bind_i3.sh
+++ scripts/bind_i3.sh
@@ -11,4 +11,4 @@ read -p "Enter the absolute config path: " config_path
 
 [[ -z $config_path ]] && echo -e "${RED}empty config path${NC}" && exit 1;
 
-echo -e "#--- SPOTIFY ------------------------------------\n# play next\nbindsym F9 exec spotifyctl.sh next\n# next and pause\nbindsym F8 exec spotifyctl.sh next-pause\n# play prev\nbindsym F7 exec spotifyctl.sh prev\n# play/pause\nbindsym F6 exec spotifyctl.sh play --open\n# show art/name/artist in notify\nbindsym F5 exec spotifyctl.sh notify" >> $config_path
+echo -e "\n#--- SPOTIFY ------------------------------------\n# play next\nbindsym F9 exec spotifyctl.sh next\n# next and pause\nbindsym F8 exec spotifyctl.sh next-pause\n# play prev\nbindsym F7 exec spotifyctl.sh prev\n# play/pause\nbindsym F6 exec spotifyctl.sh play --open\n# show art/name/artist in notify\nbindsym F5 exec spotifyctl.sh notify" >> $config_path
